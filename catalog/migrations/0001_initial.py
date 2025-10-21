12aa8f205871f99a057e26e71a9fa530a6a040ce
Makhmutov.O <olegmahmutov908@gmail.com>
2025-10-21 14:10:39 +0300

    version_8_with_orm_schablon_fbv_cbv

---
+++ catalog/migrations/0001_initial.py
@@ -0,0 +1,43 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255, unique=True, verbose_name='Наименование')),
+                ('description', models.TextField(blank=True, verbose_name='Описание')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Дата изменения')),
+            ],
+            options={
+                'verbose_name': 'Категория',
+                'verbose_name_plural': 'Категории',
+                'ordering': ['name'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255, verbose_name='Наименование')),
+                ('description', models.TextField(blank=True, verbose_name='Описание')),
+                ('image', models.ImageField(blank=True, null=True, upload_to='products/', verbose_name='Изображение')),
+                ('price', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Цена за покупку')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Дата изменения')),
+                ('category', models.ForeignKey(on_delete=models.CASCADE, related_name='products', to='catalog.category', verbose_name='Категория')),
+            ],
+            options={
+                'verbose_name': 'Продукт',
+                'verbose_name_plural': 'Продукты',
+                'ordering': ['name'],
+            },
+        ),
+    ]
